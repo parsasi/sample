148c0c4116fedc515727baaee4a9a9fa17d402b2
parsasi <mail@parsa.pro>
2025-10-19 15:52:38 -0700

    CLI-generated

---
+++ chapter-1.md
@@ -0,0 +1,11 @@
+---
+title: chapter 1
+index: 0
+author: parsa
+date: "2025-09-03"
+updated: "2025-09-03"
+---
+
+# Welcome to Slides
+
+A terminal based presentation tool. Some dummy changes. Retrying.
---
+++ sst.config.ts
@@ -0,0 +1,24 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "aws-static-site",
+      home: "aws",
+      removal: input?.stage === "production" ? "retain" : "remove",
+    };
+  },
+  async run() {
+    try {
+      return new sst.aws.StaticSite("MySite", {
+        build: {
+          command: "npx metrists build -o .metrists",
+          output: ".metrists",
+        },
+      });
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  },
+});
